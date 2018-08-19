e806db6fb5bb31ed6ae9d6f38b75b807a6df10da
aragaer <aragaer@gmail.com>
2018-08-19 17:08:28 +0300

    Change message format
    Also await and respond in Russian

--- run.lisp
+++ run.lisp
@@ -10,14 +10,14 @@
 (in-package :com.aragaer.pa-brain)
 
 (defun get-reply (message)
-  (if (string= message "hello")
-      "hello"
+  (if (string= message "Привет")
+      "Ой, приветик!"
       "unknown"))
 
 (loop for request = (json:decode-json)
-   for reply = (get-reply (cdr (assoc :text request)))
+   for reply = (get-reply (cdr (assoc :message request)))
    do (json:with-object ()
-	(json:encode-object-member :text reply)
+	(json:encode-object-member :message reply)
 	(json:as-object-member (:from) (json:encode-json (cdr (assoc :to request))))
 	(json:as-object-member (:to) (json:encode-json (cdr (assoc :from request)))))
    do (format t "~%"))
